8c5e21bba400505b53e8ac4d4395f24a0e2dba59
Andy <xtermnater@gmail.com>
2016-12-19 18:10:17 -0800

    fix andy's dumbness

--- flight-input.c
+++ flight-input.c
@@ -6,6 +6,18 @@
 Controls g_controls;
 pthread_mutex_t g_control_lock;
 
+int is_int(char *check)
+{
+	char *c = check;
+	while (*c != 0) {
+		if (*c > 57 || *c < 48) {
+			c++
+		}
+		else return 0;
+	}
+	return 1;
+}
+
 void *start_inout()
 {
 	char buffer[100];
@@ -20,16 +32,7 @@ void *start_inout()
 		sscanf(buffer, "%20s %20s", &command, &arg);
 		printf("Input received: command=\"%s\" arg=\"%s\"\n", command, arg);
 		if (strcmp(command, "throttle") == 0) {
-			int is_int = 1;
-			char *c = arg;
-			while (*c != 0) {
-				if (*c > 57 || *c < 48) {
-					is_int = 0;
-					break;
-				}
-				c++;
-			}
-			if (is_int) {
+			if (is_int(arg)) {
 				pthread_mutex_lock(&g_control_lock);
 				g_controls.throttle = 2*strtol(arg, NULL, 10);
 				pthread_mutex_unlock(&g_control_lock);
@@ -39,7 +42,7 @@ void *start_inout()
 			}
 		}
 		else if (strcmp(command, "trim_p") == 0) {
-			if (is_int) {
+			if (is_int(arg)) {
 				pthread_mutex_lock(&g_control_lock);
 				g_controls.pitch = strtol(arg, NULL, 10);
 				pthread_mutex_unlock(&g_control_lock);
@@ -49,7 +52,7 @@ void *start_inout()
 			}
 		}
 		else if (strcmp(command, "trim_r") == 0)	{
-			if(is_int){
+			if(is_int(arg)){
 				pthread_mutex_lock(&g_control_lock);
 				g_controls.roll = strtol(arg, NULL, 10);
 				pthread_mutex_unlock(&g_control_lock);
