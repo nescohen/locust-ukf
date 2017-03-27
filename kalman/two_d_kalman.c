2b275058da26e1816ac34ec549f7e49eafa55286
Nes Cohen <nsar.cohen@gmail.com>
2017-03-26 23:21:13 -0700

    fully implement kalman filter for arbitrary dimensions, need to debug tho

--- kalman/two_d_kalman.c
+++ kalman/two_d_kalman.c
@@ -3,32 +3,39 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <gsl/gsl_linalg.h>
 
 void matrix_cross_vector(double *matrix, double *vector, double *result, int rows, int columns)
 // vector columns must equal matrix rows, result must be vector of with same number of rows of matrix
 {
+	size_t result_size = sizeof(double)*rows*columns;
+	double *result_temp = alloca(result_size);
 	int i, j;
 	for (i = 0; i < rows; i++) {
-		result[i] = 0;
+		result_temp[i] = 0;
 		for (j = 0; j < columns; j++) {
-			result[i] += matrix[i*columns + j]*vector[j];
+			result_temp[i] += matrix[i*columns + j]*vector[j];
 		}
 	}
+	memcpy(result, result_temp, result_size);
 }
 
 void matrix_cross_matrix(double *a, double *b, double *result, int m, int n, int p)
 // matrix a has dimensions m x n, matrix b has dimensions n x p, and the result matrix has dimensions m x p
 {
+	size_t result_size = m*p*sizeof(double);
+	double *result_temp = alloca(result_size);
 	int i, j, k;
 	for (i = 0; i < m; i++) {
 		for (j = 0; j < p; j++) {
-			result[i*p + j] = 0;
+			result_temp[i*p + j] = 0;
 			for (k = 0; k < n; k++) {
-				result[i*p + j] += a[i*n + k]*b[k*p +j];
+				result_temp[i*p + j] += a[i*n + k]*b[k*p +j];
 			}
 		}
 	}
+	memcpy(result, result_temp, result_size);
 }
 
 void matrix_plus_matrix(double *a, double *b, double *result, int rows, int columns, int sign)
@@ -41,7 +48,7 @@ void matrix_plus_matrix(double *a, double *b, double *result, int rows, int colu
 	}
 }
 
-void matrix_quick_print(double *matrix, int row, int columns)
+void matrix_quick_print(double *matrix, int rows, int columns)
 {
 	int i, j;
 	for (i = 0; i < rows; i++) {
@@ -56,12 +63,15 @@ void matrix_quick_print(double *matrix, int row, int columns)
 void matrix_transpose(double *matrix, double *result, int rows, int columns)
 // 'rows' and 'columns' refers to the rows and columns of the input, obviously the transpose will have opposite
 {
+	size_t result_size = sizeof(double)*rows*columns;
+	double *result_temp = alloca(result_size);
 	int i, j;
 	for (i = 0; i < columns; i++) {
 		for (j = 0; j < rows; j++) {
-			result[i*rows + j] = matrix[j*columns+i];
+			result_temp[i*rows + j] = matrix[j*columns+i];
 		}
 	}
+	memcpy(result, result_temp, result_size);
 }
 
 void matrix_inverse(double *matrix, double *result, int size)
@@ -79,10 +89,23 @@ void matrix_inverse(double *matrix, double *result, int size)
 	gsl_permutation_free(p);
 }
 
+void matrix_identity(double *matrix, int size)
+{
+	int i, j;
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (i == j) matrix[i*size + j] = 1;
+			else matrix[i*size + j] = 0;
+		}
+	}
+}
+
 void predict(double *x, double *P, double *F, double *Q, double *x_f, double *P_f, int x_dim)
 {
+	// x = Fx + Bu
 	matrix_cross_vector(F, x, x_f, x_dim, x_dim);
 
+	// P = FPF^t + Q
 	double *transpose = alloca(x_dim*sizeof(double));
 	matrix_transpose(F, transpose, x_dim, x_dim);
 
@@ -94,21 +117,33 @@ void predict(double *x, double *P, double *F, double *Q, double *x_f, double *P_
 
 void update(double *x, double *z, double *P, double *H, double *R, double *x_f, double *P_f, int x_dim, int z_dim)
 {
+	// y = z-Hx
 	double *y = alloca(z_dim*sizeof(double));
 	double *temp_v = alloca(z_dim*sizeof(double));
-
 	matrix_cross_vector(H, x, temp_v, x_dim, z_dim);
-	matrix_plus_matrix(z, temp_v, y, z_dim, 1);
+	matrix_plus_matrix(z, temp_v, y, z_dim, 1, 1);
 
-	double *K = alloca(z_dim*z_dim*sizeof(double));
-	double *H_t = allocate(x_dim*z_dim*sizeof(double));
-	double *temp_m_a = alloca(z_dim*z_dim*sizeof(double));
-	double *temp_m_b = alloca(z_dim*z_dim*sizeof(double));
-
+	// K = PH^t(HPH^t + R)^-1
+	double *K = alloca(x_dim*z_dim*sizeof(double));
+	double *temp_k = alloca(z_dim*z_dim*sizeof(double));
+	double *H_t = alloca(x_dim*z_dim*sizeof(double));
 	matrix_transpose(H, H_t, z_dim, z_dim);
-	matrix_cross_matrix(P, H_t, temp_m_a, x_dim, x_dim, z_dim);
+	matrix_cross_matrix(H, P, K, z_dim, x_dim, x_dim);
+	matrix_cross_matrix(K, H_t, temp_k, z_dim, x_dim, z_dim); 
+	matrix_plus_matrix(temp_k, R, temp_k, z_dim, z_dim, 1);
+	matrix_inverse(temp_k, temp_k, z_dim);
+	matrix_cross_matrix(P, H_t, K, x_dim, x_dim, z_dim);
+	matrix_cross_matrix(K, temp_k, K, x_dim, z_dim, z_dim);
 
-	//TODO: finish this implementation! good work, you're getting close
+	// x = x + Ky
+	matrix_cross_vector(K, y, x_f, x_dim, z_dim);
+	matrix_plus_matrix(x, x_f, x_f, x_dim, 1, 1);
+
+	// P = (I - KH)P
+	double *identity = alloca(x_dim*x_dim*sizeof(double));
+	matrix_cross_matrix(K, H, P_f, x_dim, z_dim, x_dim);
+	matrix_plus_matrix(identity, P_f, P_f, x_dim, x_dim, 0);
+	matrix_cross_matrix(P_f, P, P_f, x_dim, x_dim, x_dim);
 }
 
 void generate_transform(double delta_t, double *result)
@@ -124,16 +159,26 @@ int main()
 	double state[2] = {10, 4.5}; // x, 2-vector to represent current state with position(0) and velocity(1)
 	double state_covariance[4] = {500, 0, 0, 49}; // P, state covariance, 2x2 matrix
 	double process_noise[4] = {0, 0, 0, 0}; // Q, for now we have no process nois
-
-	double state_transition[4];
+	double m_function[2] = {1, 0}; // H, only measuring position
+	double m_covariance[1] = {5}; // R
+	double state_transition[4]; // F
+	double prior[2];
+	double posterior[2];
+	double prior_covariance[4];
+	double posterior_covariance[4];
+	double measurements[10] = {10, 10.45, 11, 12, 13, 14, 16, 17, 19, 21.5};
 	generate_transform(0.1, state_transition);
 
-	double prior[2];
-	double prior_covariance[4];
-	predict(state, state_covariance, state_transition, process_noise, prior, prior_covariance, 2);
-	printf("assumption = {%f, %f}\n", state[0], state[1]);
-	printf("prior = {%f, %f}\n", prior[0], prior[1]);
-	printf("covariance = {%f, %f, %f, %f}\n", prior_covariance[0], prior_covariance[1], prior_covariance[2], prior_covariance[3]);
+	int i;
+	for (i = 0; i < 10; i++) {
+		predict(state, state_covariance, state_transition, process_noise, prior, prior_covariance, 2);
+		update(prior, &measurements[i], prior_covariance, m_function, m_covariance, posterior, posterior_covariance, 2, 1);
+		memcpy(state, posterior, sizeof(state));
+		memcpy(state_covariance, posterior_covariance, sizeof(state_covariance));
+	}
+
+	matrix_quick_print(posterior, 2, 1);
+	matrix_quick_print(posterior_covariance, 2, 2);
 
 	return 0;
 }
