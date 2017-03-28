b90efa57a4f1838d82f9939afeee3aa44e4b9d46
Nes Cohen <nsar.cohen@gmail.com>
2017-03-28 15:22:14 -0700

    add todos

--- kalman/two_d_kalman.c
+++ kalman/two_d_kalman.c
@@ -160,8 +160,10 @@ void generate_transform(double delta_t, double *result)
 
 int main()
 {
-	double state[2] = {10, 4.5}; // x, 2-vector to represent current state with position(0) and velocity(1)
-	double state_covariance[4] = {500, 0, 0, 49}; // P, state covariance, 2x2 matrix
+	// TODO: seperate all functions required by the kalman filter to a seperate file and header
+
+	double state[2] = {10, 1}; // x, 2-vector to represent current state with position(0) and velocity(1)
+	double state_covariance[4] = {500, 0, 0, 10}; // P, state covariance, 2x2 matrix
 	double process_noise[4] = {0, 0, 0, 0}; // Q, for now we have no process noise
 	double m_function[2] = {1, 0}; // H, only measuring position
 	double m_covariance[1] = {5}; // R
@@ -170,9 +172,9 @@ int main()
 	double posterior[2];
 	double prior_covariance[4];
 	double posterior_covariance[4];
-	double measurements[10] = {10, 10.45, 11, 12, 13, 14, 16, 17, 19, 21.5};
+	double measurements[10] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
-	generate_transform(0.1, state_transition);
+	generate_transform(1, state_transition);
 
 	int i;
 	for (i = 0; i < 10; i++) {
