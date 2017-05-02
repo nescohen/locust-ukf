5c828e1ec24d87b48389270c0aa703e5cfb0462c
Nes Cohen <nsar.cohen@gmail.com>
2017-05-01 20:01:02 -0700

    clean up unused files in /kalman

--- kalman/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc -g -Wall -o test.bin kalman_test.c quaternion_util.c -lm
--- kalman/proto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-gcc -Wall -g -O0 -o ukf_quat.bin ukf_test.c kalman.c matrix_util.c quaternion_util.c -lgsl -lgslcblas -lm
-valgrind --track-origins=yes --log-file=test_err.txt --tool=memcheck -- ./ukf_quat.bin > test_out.txt
