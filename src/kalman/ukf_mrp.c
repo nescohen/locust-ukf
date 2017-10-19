fa411e85f15b74328177174188c12559c36b9674
Nes Cohen <nsar.cohen@gmail.com>
2017-10-19 15:57:33 -0700

    slight kalman modifications and testing

--- src/kalman/ukf_mrp.c
+++ src/kalman/ukf_mrp.c
@@ -236,9 +236,8 @@ void custom_scaled_points(double *x, double *P, double *chi, int n, double a, do
 
 #ifdef DEBUG
 	//debug printing for testing
-	printf("SIGMA POINTS\n");
-	matrix_quick_print(chi, 2*n+1, n);
-	printf("\n");
+	// printf("SIGMA POINTS\n");
+	// matrix_quick_print(chi, 2*n+1, n);
 #endif
 
 }
