9347b2b3f8ff4d4b135613fa0f48837e57665c66
Nes Cohen <nsar.cohen@gmail.com>
2017-11-27 16:34:56 -0800

    working on seperate network client module

---
+++ src/client/client.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+// network related definitions
+#define NETWORK_THROTTLE 1
+#define NETWORK_OFF 2
+
+void *network_client_start(void *arg);
+
+int establish_connection();
+
+#endif
